1b8dcda2167f7486899bb6a8e12f4f585e72d6f0
fmnoise <fmnoise@gmail.com>
2018-07-28 23:25:17 +0200

    Add url to project

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject dawcs/flow "0.1.0"
   :description "Handling exceptions in functional way"
+  :url "https://github.com/dawcs/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
