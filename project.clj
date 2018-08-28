bb1d7abe1507ad742b82a15d885fff405c1e0752
fmnoise <fmnoise@gmail.com>
2018-08-28 18:20:24 +0300

    Version 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dawcs/flow "0.1.1"
+(defproject dawcs/flow "0.1.2"
   :description "Declarative exceptions handling"
   :url "https://github.com/dawcs/flow"
   :license {:name "Eclipse Public License"
