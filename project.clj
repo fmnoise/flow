84d8bbd46090563015f58dc3f650e359f6663253
fmnoise <fmnoise@gmail.com>
2019-02-01 13:06:59 +0200

    Configure java sources

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/dawcs/flow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :java-source-paths ["src/dawcs/flow"]
+  :javac-options ["-target" "1.7", "-source" "1.7"])
