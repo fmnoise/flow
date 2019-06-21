e2f92394d75465e3311c3b6183d86f87342e1e8f
fmnoise <fmnoise@gmail.com>
2019-06-21 21:36:42 +0300

    Add lein-doo plugin

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-doo "0.1.11"]]
   :java-source-paths ["src/dawcs/flow"]
   :javac-options ["-target" "1.7", "-source" "1.7"])
