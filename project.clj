a7f8d44bc9eb4c105158c1498e87205f120c50a4
fmnoise <fmnoise@gmail.com>
2021-10-20 00:40:28 +0300

    Remove 1.7 java target

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[lein-doo "0.1.11"]]
-  :java-source-paths ["src/fmnoise/flow"]
-  :javac-options ["-target" "1.7", "-source" "1.7"])
+  :java-source-paths ["src/fmnoise/flow"])
